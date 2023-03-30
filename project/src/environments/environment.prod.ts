2764f7f99e93667921fa96eea48a011acf52a611
Cezar Fuhr <cezar@nextar.com.br>
2023-03-29 23:53:31 -0300

    update use secrets

--- project/src/environments/environment.prod.ts
+++ project/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import { environment as base } from './environment';
 
 export const environment = {
+    ...base,
     production: true,
     marvelPrivateKey: process.env.MARVEL_PRIVATE_KEY,
     marvelPublicKey: process.env.MARVEL_PUBLIC_KEY,
