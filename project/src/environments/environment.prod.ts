7344cd3e1d9007c989d64af4a90f1c174622f50d
Cezar Fuhr <cezar@nextar.com.br>
2023-03-30 00:02:00 -0300

    update use secrets

--- project/src/environments/environment.prod.ts
+++ project/src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
-import { environment as base } from './environment';
-
 export const environment = {
-    ...base,
     production: true,
-    marvelPrivateKey: process.env.MARVEL_PRIVATE_KEY,
-    marvelPublicKey: process.env.MARVEL_PUBLIC_KEY,
+    marvelPrivateKey: 'MARVEL_PRIVATE_KEY',
+    marvelPublicKey: 'MARVEL_PUBLIC_KEY',
     backendUrl: 'https://simone.codenoob.dev/api/v1/',
     pokeapi: 'https://pokeapi.co/api/v2/',
     marvel: 'https://gateway.marvel.com/v1/',
