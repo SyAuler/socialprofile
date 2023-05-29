4c3c54dc1cffa281b7a8c201896de14107882ba7
Simone Auler <auler.simone@gmail.com>
2023-05-28 22:43:21 -0300

    WIP - week component

--- project/src/app/week-planner/week-planner.component.ts
+++ project/src/app/week-planner/week-planner.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class WeekPlannerComponent {
 
+    data: any = [
+        {
+            id: 1,
+            name: 'Mussum Ipsum, cacilds vidis litro abertis. In elementis mé pra quem é amistosis quis leo.Delegadis gente finis, bibendum egestas augue arcu ut est.',
+        },
+    ]
+
+    teste: any = [
+        {
+            id: 1,
+            name: 'Mussum Ipsum, cacilds vidis litro abertis.',
+        },
+        {
+            id: 2,
+            name: 'Mussum Ipsum, cacilds vidis litro abertis. In elementis mé pra quem é amistosis quis leo.Delegadis gente finis, ',
+        },
+    ]
+    
+    teste1: any = [
+        {
+            id: 1,
+            name: 'Mussum Ipsum, cacilds vidis litro abertis.',
+        },        
+        {
+            id: 2,
+            name: 'Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!',
+        },
+        {
+            id: 2,
+            name: 'Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!',
+        },
+    ]
 }
